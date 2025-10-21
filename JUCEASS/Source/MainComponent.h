86004789d50d9f4e3f4cbb58ce138c39a9c461c2
mosafwat77 <msafwat2004mohamed@gmail.com>
2025-10-21 18:13:31 +0300

    Fix include case sensitivity and class name

--- JUCEASS/Source/MainComponent.h
+++ JUCEASS/Source/MainComponent.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <JuceHeader.h>
-#include "PlayerGUI.h"
+#include "PlayerGui.h"
 
 class MainComponent : public juce::AudioAppComponent
 {
@@ -8,13 +8,15 @@ public:
     MainComponent();
     ~MainComponent() override;
 
+    // JUCE Audio callbacks
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
+
     void resized() override;
 
 private:
-    PlayerGUI playerGUI;
+    PlayerGui playerGUI; 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
