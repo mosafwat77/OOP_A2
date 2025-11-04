c82644ba7eea601bd940f4906b34614b1eb1adde
mosafwat77 <msafwat2004mohamed@gmail.com>
2025-11-04 19:50:15 +0200

    Enhance PlayerGui with marker management features
    Added ListBoxModel functionality for marker management.

--- PlayerGui.h
+++ PlayerGui.h
@@ -6,7 +6,8 @@
 class PlayerGui : public juce::AudioAppComponent,
     public juce::Button::Listener,
     public juce::Slider::Listener,
-    public juce::Timer
+    public juce::Timer,
+    public juce::ListBoxModel
 {
 public:
     PlayerGui();
@@ -20,17 +21,47 @@ public:
     void releaseResources() override;
 
 private:
+  
     void buttonClicked(juce::Button* button) override;
     void sliderValueChanged(juce::Slider* slider) override;
     void timerCallback() override;
-
-    juce::String formatTime(double seconds);
-
     void setLoopStart();
     void setLoopEnd();
     void clearLoopMarkers();
 
+    struct Marker
+    {
+        double time;
+        juce::String label;
+    };
+
+    void refreshMarkerList();
+    void sortMarkers();
+    void addMarkerAtCurrentPosition();
+    void deleteSelectedMarker();
+    void gotoSelectedMarker();
+
+    int getNumRows() override;
+    void paintListBoxItem(int rowNumber, juce::Graphics& g, int width, int height, bool rowIsSelected) override;
+    void listBoxItemDoubleClicked(int row, const juce::MouseEvent&) override;
+    void deleteKeyPressed(int lastRowSelected) override;
+
+    juce::String formatTime(double seconds)
+    {
+        int mins = (int)(seconds / 60.0);
+        int secs = (int)std::fmod(seconds, 60.0);
+        return juce::String::formatted("%d:%02d", mins, secs);
+    }
+
     Playeraudio playerAudio;
+    bool fileLoaded = false;
+    float previousVolume = 0.5f;
+
+    double loopStartTime = 0.0;
+    double loopEndTime = 0.0;
+    bool loopStartSet = false;
+    bool loopEndSet = false;
+    bool abLoopEnabled = false;
 
     juce::TextButton loadButton;
     juce::TextButton playPauseButton;
@@ -41,9 +72,14 @@ private:
     juce::TextButton loopStartButton{ "A" };
     juce::TextButton loopEndButton{ "B" };
     juce::TextButton clearLoopButton{ "Clear Loop" };
-    juce::TextButton forwardButton{ "|> " };
-    juce::TextButton backwardButton{ "|< " };
+    juce::TextButton forwardButton{ ">>" };
+    juce::TextButton backwardButton{ "<<" };
+    juce::TextButton addMarkerButton{ "Add Marker" };
+    juce::TextButton deleteMarkerButton{ "Delete Marker" };
+    juce::TextButton gotoMarkerButton{ "Go To Marker" };
 
+    juce::ListBox markerList;
+    std::vector<Marker> markers;
 
     juce::Slider volumeSlider;
     juce::Slider speedSlider;
@@ -58,15 +94,6 @@ private:
     juce::SpinLock thumbnailLock;
 
     std::unique_ptr<juce::FileChooser> fileChooser;
-    bool fileLoaded = false;
-    float previousVolume = 0.5f; 
-
-
-    double loopStartTime = 0.0;
-    double loopEndTime = 0.0;
-    bool loopStartSet = false;
-    bool loopEndSet = false;
-    bool abLoopEnabled = false;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGui)
 };
