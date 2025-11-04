953fcaa73fc12bf77bf12c783f03879c8b4633e2
mosafwat77 <msafwat2004mohamed@gmail.com>
2025-11-04 12:49:56 +0200

    Refactor PlayerGui to add marker functionality
    Updated PlayerGui class to include ListBoxModel and ChangeListener interfaces, added marker controls, and modified existing methods.

--- PlayerGui.h
+++ PlayerGui.h
@@ -1,12 +1,13 @@
 #pragma once
-
 #include <JuceHeader.h>
 #include "Playeraudio.h"
 
-class PlayerGui : public juce::AudioAppComponent,
+class PlayerGui : public juce::Component,
     public juce::Button::Listener,
     public juce::Slider::Listener,
-    public juce::Timer
+    public juce::Timer,
+    public juce::ListBoxModel,
+    public juce::ChangeListener
 {
 public:
     PlayerGui();
@@ -14,23 +15,25 @@ public:
 
     void paint(juce::Graphics&) override;
     void resized() override;
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
+    void releaseResources();
 
-    void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
-    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
-    void releaseResources() override;
-
-private:
     void buttonClicked(juce::Button* button) override;
     void sliderValueChanged(juce::Slider* slider) override;
     void timerCallback() override;
+    void changeListenerCallback(juce::ChangeBroadcaster* source) override;
 
-    juce::String formatTime(double seconds);
+    int getNumRows() override;
+    void paintListBoxItem(int rowNumber, juce::Graphics& g, int width, int height, bool rowIsSelected) override;
+    void listBoxItemClicked(int row, const juce::MouseEvent&) override;
+    void deleteKeyPressed(int lastRowSelected) override;
 
-    void setLoopStart();
-    void setLoopEnd();
-    void clearLoopMarkers();
-
+private:
     Playeraudio playerAudio;
+    juce::AudioThumbnailCache thumbnailCache;
+    juce::AudioThumbnail thumbnail;
+    juce::SpinLock thumbnailLock;
 
     juce::TextButton loadButton;
     juce::TextButton playPauseButton;
@@ -38,33 +41,45 @@ private:
     juce::TextButton restartButton;
     juce::TextButton muteButton;
     juce::TextButton loopButton;
-    juce::TextButton loopStartButton{ "A" };
-    juce::TextButton loopEndButton{ "B" };
-    juce::TextButton clearLoopButton{ "Clear Loop" };
-    juce::TextButton forwardButton{ "|> " };
-    juce::TextButton backwardButton{ "|< " };
+    juce::TextButton loopStartButton;
+    juce::TextButton loopEndButton;
+    juce::TextButton clearLoopButton;
+    juce::TextButton forwardButton{ ">|" };
+    juce::TextButton backwardButton{" |<"};
 
+    // Marker controls
+    juce::TextButton addMarkerButton;
+    juce::TextButton deleteMarkerButton;
+    juce::ListBox markerListBox;
+    juce::Label markerLabel;
 
     juce::Slider volumeSlider;
     juce::Slider speedSlider;
     juce::Slider positionSlider;
-
     juce::Label currentTimeLabel;
     juce::Label totalTimeLabel;
     juce::Label speedLabel;
-
-    juce::AudioThumbnailCache thumbnailCache;
-    juce::AudioThumbnail thumbnail;
-    juce::SpinLock thumbnailLock;
+    juce::Label volumelabel;
 
     std::unique_ptr<juce::FileChooser> fileChooser;
     bool fileLoaded = false;
 
-    double loopStartTime = 0.0;
-    double loopEndTime = 0.0;
     bool loopStartSet = false;
     bool loopEndSet = false;
+    double loopStartTime = 0.0;
+    double loopEndTime = 0.0;
     bool abLoopEnabled = false;
+
+    int selectedMarkerRow = -1;
+
+    void setLoopStart();
+    void setLoopEnd();
+    void clearLoopMarkers();
+    juce::String formatTime(double seconds);
+
+    void addMarker();
+    void deleteSelectedMarker();
+    void jumpToMarker(int markerIndex);
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGui)
 };
