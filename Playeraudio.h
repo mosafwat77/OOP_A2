b52af7dbcb3558dc6d74c3045e208b633d3fdfe9
mosafwat77 <msafwat2004mohamed@gmail.com>
2025-11-05 23:51:59 +0200

    Create Playeraudio.h

---
+++ Playeraudio.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <JuceHeader.h>
+
+class Playeraudio
+{
+public:
+    Playeraudio();
+    ~Playeraudio();
+
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
+    void releaseResources();
+
+    void loadFile(const juce::File& audioFile);
+    void start();
+    void stop();
+    void pause();
+    void restart();
+
+    void setGain(float gain);
+    void mute();
+    void unmute();
+    bool isMuted() const;
+
+    void increment(double seconds);
+    void decrement(double seconds);
+
+    void setPosition(double positionInSeconds);
+    double getPosition() const;
+    double getLength() const;
+
+    void setLooping(bool shouldLoop);
+    bool isLoopingEnabled() const { return isLooping; }
+    bool isPlaying() const;
+    void enableLooping(bool shouldLoop);
+    bool isTrackPlaying() const;
+    double getPlaybackTime() const;
+    double getTrackDuration() const;
+    juce::AudioFormatManager& getFormatManager() { return formatManager; }
+
+    void setSpeed(double ratio);
+    float getCurrentGain() const { return muted ? lastGain : transportSource.getGain(); }
+
+private:
+    juce::AudioFormatManager formatManager;
+    juce::AudioTransportSource transportSource;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::ResamplingAudioSource resampleSource;
+
+    bool muted = false;
+    bool isLooping = false;
+    float lastGain = 1.0f;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Playeraudio)
+};
